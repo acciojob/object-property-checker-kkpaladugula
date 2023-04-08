0178fb3eca3aefc5c9eafd7da811a2496721ea16
aami_mm <mukarramos@outlook.com>
2023-04-08 17:44:09 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+const sampleObject = { red: "#FF0000", 
+                        green: "#00FF00", 
+                        white: "#FFFFFF" 
+                      };
 
 function hasKey(key) {
-  //   write your code here
+  if(key==sampleObject.red || key==sampleObject.green||key==sampleObject.white){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 // Do not change the code below
